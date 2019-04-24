944b7fc8b0ba5d3442a6f0fe5525c17e8514e9ca
nicolehuey <41655015+nicolehuey@users.noreply.github.com>
2019-04-24 14:17:58 +1000

    still havent pass test 6

--- Dijkstra.c
+++ Dijkstra.c
@@ -12,23 +12,26 @@ ShortestPaths dijkstra(Graph g, Vertex v) {
 	 PQ q = newPQ();
 	 int nV = numVerticies(g);
 
-   int visited[nV];
-	 for (int i = 0;i < nV;i++){
-		 visited[i] = 0;
-	 }
+   // int visited[nV];
+	 // for (int i = 0;i < nV;i++){
+		//  visited[i] = 0;
+	 // }
 	 ShortestPaths* path = malloc(sizeof (ShortestPaths));
 	 path->noNodes = nV;
 	 path->src = v;
 	 path->dist = malloc(nV* sizeof (int));
 	 path->pred = malloc(nV * sizeof (PredNode *));
 
-
-
-
 	// intialise all the dist and prev for vertex in Graph
 	for (int i = 0;i < nV;i++){
-			path->dist[i] = 0;
+			path->dist[i] = INT_MAX;
 			path->pred[i] = NULL;
+			ItemPQ vertex;
+			if (i != v){
+				vertex.key = i;
+				vertex.value = 100;
+				addPQ(q,vertex);
+			}
 	}
 
 	path->pred[v]= NULL;
@@ -40,75 +43,55 @@ ShortestPaths dijkstra(Graph g, Vertex v) {
 	source.value = 0;
 	addPQ(q,source);
 
-	visited [source.key] = 1;
-
+	//visited [source.key] = 1;
+	int n = 0;
 	while (!PQEmpty(q)) {
 		//  deq the smallest v
 		ItemPQ deq = dequeuePQ(q);
 		// // check adjacent Nodes
-	  //printf("deq item is {%d,%d}\n",deq.key,deq.value);
+		if (path->dist[deq.key] == INT_MAX){
+			//printf("change to 0\n");
+			path->dist[deq.key] = 0;
+			n = 1;
+		//	printf("path->dist[%d] = %d\n",i,path->dist[i]);
+		}
 	  AdjList list = outIncident(g,deq.key);
 
 		// fill adjacent nodes into q
-			while (list != NULL){
+			while (list != NULL && n == 0){
 				// put into item
 				ItemPQ adjitem;
 				adjitem.key = list->w;
 				adjitem.value = list->weight;
 
-				if (visited[adjitem.key] == 0){
-					addPQ(q,adjitem);
-					//printf("adj item is {%d,%d}\n",adjitem.key,adjitem.value);
-					visited[adjitem.key] = 1;
-					// insert the distance
-					if (deq.key == source.key){
-						path->dist[adjitem.key] = adjitem.value;
-							//printf("path->dist[%d] is %d\n",adjitem.key,path->dist[adjitem.key]);
-					} else {
-						//printf("adjitem.value %d + path[dist] value %d\n",adjitem.value, path->dist[deq.key]);
-						path->dist[adjitem.key] = adjitem.value + path->dist[deq.key];
-						//printf("path->dist[%d] is %d\n",adjitem.key,path->dist[adjitem.key]);
-					}
-
-					// add predecessor node
-						PredNode* node = malloc (sizeof(node));
-						node->v = deq.key;
-						node->next = NULL;
-						path->pred[adjitem.key] = node;
-
-				} else if (visited[adjitem.key] == 1){
-					  //printf("visited adjitem.key %d\n",adjitem.key);
-
-						int alt = adjitem.value + path->dist[deq.key];
-						//printf("comapre with %d\n",path->dist[adjitem.key] );
-						//printf("alt = %d + %d\n",adjitem.value,path->dist[deq.key]);
-						if (path->dist[adjitem.key] > alt){
-							//printf("ALT IS SMALLER\n");
-							path->dist[adjitem.key] = alt;
-							// use update pq
-							ItemPQ upditem;
-							upditem.key = adjitem.key;
-							upditem.value = adjitem.value;
-							updatePQ(q,upditem);
-							PredNode* node = malloc (sizeof(node));
-							node->v = deq.key;
-							node->next = NULL;
-							path->pred[adjitem.key] = node;
-
-						} else if (path->dist[adjitem.key] == alt){
-								PredNode *new = malloc (sizeof(new));
-								new->v = deq.key;
-								new->next = path->pred[adjitem.key];
-								path->pred[adjitem.key] = new;
-
-						}
+				int alt = path->dist[deq.key] + adjitem.value;
+				//printf("alt %d = deq.key [%d] %d + adjitem [%d] %d\n",
+				//malt,deq.key,path->dist[deq.key],adjitem.key,adjitem.value );
+				if (alt < path->dist[adjitem.key]){
+					path->dist[adjitem.key] = alt;
+					PredNode* node = malloc (sizeof(node));
+					node->v = deq.key;
+					node->next = NULL;
+					path->pred[adjitem.key] = node;
+					ItemPQ upditem;
+					upditem.key = adjitem.key;
+					upditem.value = adjitem.value;
+					updatePQ(q,upditem);
+				} else if (alt == path->dist[adjitem.key]){
+					PredNode *new = malloc (sizeof(new));
+					new->v = deq.key;
+					new->next = path->pred[adjitem.key];
+					path->pred[adjitem.key] = new;
+					// ItemPQ upditem;
+					// upditem.key = adjitem.key;
+					// upditem.value = adjitem.value;
+					// updatePQ(q,upditem);
 				}
 				list = list->next;
 
 			}
 
 		}
-
 
 	return *path;
 }
